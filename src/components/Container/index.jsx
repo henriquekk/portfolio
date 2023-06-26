da31dcf12b3e963787b70c0fe3ea9b76cbd303f6
henriquekk <henrique.guerrato@gmail.com>
2023-06-26 17:25:43 -0300

    component Container

---
+++ src/components/Container/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Container.module.scss';
+
+export default function Container({ children }) {
+  return (
+    <div className={styles.container}>
+      {children}
+    </div>
+  )
+}
