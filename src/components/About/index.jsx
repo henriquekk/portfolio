f841c86abfcca79ed4ed5fe59ffa89a5563de2da
henriquekk <henrique.guerrato@gmail.com>
2023-06-27 01:25:55 -0300

    adjust About me

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './About.module.scss';
+import image from '../../assets/images/aboutme.jpg'
 
 export default function About() {
   return (
     <section className={styles.about_section} id='about'>
 
-      <img src='https://github.com/maykbrito.png'/>
+      <img src={image}/>
       <div className={styles.about_text}>
         <h1>Sobre mim</h1>
         <p>Meu nome é Henrique Guerrato, nasci dia 10 de agosto de 2000 em São Paulo - SP e atualmente estou morando em Bertioga - SP.</p> 
